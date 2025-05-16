d49efbbff43485b0eaebbcc9748f15822a08f3fc
Sagar1566 <gajbharsagar2005@gmail.com>
2025-05-16 19:02:32 +0530

    Components to Create (Basic Boilerplate)

---
+++ frontend/src/landing_page/home/HomePage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Hero from './Hero';
+import Awards from './Awards';
+import Pricing from './Pricing';
+import Education from './Education';
+import OpenAccount from '../OpenAccount';
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+import Stats from './Stats';
+
+
+
+function HomePage() {
+    return ( 
+        <>
+        <Navbar/>
+        <Hero/>
+        <Awards/>
+        <Stats/>
+        <Pricing/>
+        <Education/>
+        <OpenAccount/>
+        <Footer/>
+        </>
+     );
+}
+
+export default HomePage;
---
+++ frontend/src/landing_page/products/Univerce.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Univerce() {
+    return ( 
+        <h1>Univerce</h1>
+     );
+}
+
+export default Univerce;
