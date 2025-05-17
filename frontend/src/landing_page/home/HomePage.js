a74a0cd4fe986514bd123eefc1f9e0d853b5db24
Sagar1566 <gajbharsagar2005@gmail.com>
2025-05-17 12:38:31 +0530

    Setting Up React Router

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -4,25 +4,21 @@ import Awards from './Awards';
 import Pricing from './Pricing';
 import Education from './Education';
 import OpenAccount from '../OpenAccount';
-import Navbar from '../Navbar';
-import Footer from '../Footer';
 import Stats from './Stats';
 
 
 
 function HomePage() {
-    return ( 
+    return (
         <>
-        <Navbar/>
-        <Hero/>
-        <Awards/>
-        <Stats/>
-        <Pricing/>
-        <Education/>
-        <OpenAccount/>
-        <Footer/>
+            <Hero />
+            <Awards />
+            <Stats />
+            <Pricing />
+            <Education />
+            <OpenAccount />
         </>
-     );
+    );
 }
 
 export default HomePage;
